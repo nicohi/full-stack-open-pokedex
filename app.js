0d070aa382c8d0edc7a2a23305b3719ce807156c
Nicolas Hiillos <nicolashiillos@gmail.com>
2023-07-27 13:57:13 +0300

    Complete 11.14

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send('11.13')
+  res.send('11.14')
 })
 
 app.get('/health', (req, res) => {
